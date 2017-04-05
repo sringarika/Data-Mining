47c29093ca66fea2ef68815d16afecdd299b5d96
harycane <harycane@gmail.com>
2017-04-05 18:09:59 -0400

    Euclidean distance function

--- knn.java
+++ knn.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import weka.core.Instance;
 import weka.core.Instances;
@@ -27,8 +28,55 @@ public class knn {
   /* for(int i = 0; i < 185; i++) {
        System.out.println(trainingSet.get(i)); System.out.println(count++); 
        }*/
-    System.out.println(trainingSet.get(0).toString(1));
+    System.out.println(trainingSet.get(0));
     breader.close();
 
 }
+   
+   public int computeKnn(List<double[]> dtrain, List<double[]> dtest)  {
+       int kvote = 1;
+       for (int i = 0; i < dtest.size(); i++) {
+           double[] inverseDistanceArray = computeInverseEuclideanDistance(dtrain, dtest.get(i));
+           double[] arrayBackup = inverseDistanceArray.clone();
+           Arrays.sort(inverseDistanceArray);
+          System.arraycopy(inverseDistanceArray, 0, inverseDistanceArray, 0, 3);
+           
+       }
+       return kvote;
+   }
+   public double[] computeInverseEuclideanDistance(List<double[]> a  , double[] b) {
+       double[] d = new double[a.size()]; 
+       double similarity;
+       
+       for(int i = 0; i < b.length; i++) {
+           for (int j = 0; j < a.size(); j++) {
+              
+               
+               if (i >= 2) {
+                   
+           d[j]+= Math.pow((a.get(j)[i] - b[i]),2);
+               
+               }else {
+                       if (a.get(j)[i] == b[i]) {
+                           similarity = 1;
+                       
+                       }else {
+                       
+                           similarity = 0;
+                       }
+                  
+                       d[j]+= 1 - similarity;
+               }
+           
+             
+              d[j] = Math.sqrt(d[j]);
+              d[j] = 1.0/d[j];
+           }
+          
+       }
+       
+       
+       return d;
+   }
+   
 }
